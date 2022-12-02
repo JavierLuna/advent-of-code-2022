3c30800703e9ef50b881f99e2c839ffff9e63bc7
Javier Luna Molina <javierlunamolina@gmail.com>
2022-12-02 21:02:19 +0100

    Add day 1 and day 2

---
+++ src/day01/solution.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def read_elf_cals(file_name: str) -> List[int]:
+    elf_cals = [0]
+    with open(file_name) as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                elf_cals.append(0)
+            else:
+                elf_cals[-1] += int(line)
+    return sorted(elf_cals)
+
+
+def sum_topn_elfs(file_name: str, n: int) -> int:
+    return sum(sorted(read_elf_cals(file_name), reverse=True)[:n])
+
+
+def part1(file_name: str):
+    print(sum_topn_elfs(file_name, 1))
+
+
+def part2(file_name: str):
+    print(sum_topn_elfs(file_name, 3))
+
+
+for file_name in ["test.txt", "real.txt"]:
+    part1(f"./inputs/{file_name}")
+    part2(f"./inputs/{file_name}")
---
+++ src/day02/solution.py
@@ -0,0 +1,31 @@
+import functools
+
+from typing import List, Tuple
+
+EncryptedStrategy = List[Tuple[str, str]]
+
+@functools.cache
+def get_score(opponent: str, me: str) -> int:
+    me_index = "XYZ".index(me)
+    return me_index + 1 + ["BAC", "CBA", "ACB"][me_index].index(opponent) * 3
+
+
+@functools.cache
+def get_my_hand(opponent: str, scenario: str) -> str:
+    return ["ZXY", "XYZ", "YZX"]["XYZ".index(scenario)]["ABC".index(opponent)]
+
+
+def part_1(content: EncryptedStrategy) -> int:
+    return sum(get_score(opponent, you) for opponent, you in content)
+
+
+def part_2(content: EncryptedStrategy) -> int:
+    return sum(get_score(opponent, get_my_hand(opponent, scenario)) for opponent, scenario in content)
+
+
+for file_name in ["test", "real"]:
+    with open(f"./inputs/{file_name}.txt") as f:
+        strategy: EncryptedStrategy = [tuple(l.strip().split(" ")) for l in f]
+
+    print(f"Part 1: {part_1(strategy)}")
+    print(f"Part 2: {part_2(strategy)}")
